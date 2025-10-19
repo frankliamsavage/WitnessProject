6319c974ab406ed5e4d59f5ca0087e5f8f509647
frankliamsavage <frank.liam.savage@gmail.com>
2025-10-19 11:15:41 -0500

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,14 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: ["/", "/help", "/about"],
+  afterSignInUrl: "/dashboard",
+  afterSignUpUrl: "/dashboard",
+});
+
+export const config = {
+  matcher: [
+    "/((?!.*\\..*|_next).*)",
+    "/",
+  ],
+};

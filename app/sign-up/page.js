a783b9a733f0b5fd584c19a519345043148b363d
frankliamsavage <frank.liam.savage@gmail.com>
2025-10-19 14:07:14 -0500

    Fix sign-up/page.js

---
+++ app/sign-up/page.js
@@ -0,0 +1,10 @@
+"use client";
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div style={{ minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <SignUp />
+    </div>
+  );
+}

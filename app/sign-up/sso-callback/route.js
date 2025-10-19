5fe000f3319c2e6e5ca37f436ca309afe5e07fc6
frankliamsavage <frank.liam.savage@gmail.com>
2025-10-19 14:30:55 -0500

    Update route.js

--- app/sign-up/sso-callback/route.js
+++ app/sign-up/sso-callback/route.js
@@ -1,6 +1,5 @@
-// app/sign-in/sso-callback/route.js
-import { handleSignInCallback } from "@clerk/nextjs/server";
+import { handleOAuthCallback } from "@clerk/nextjs/server";
 
 export async function GET(req) {
-  return handleSignInCallback(req);
+  return handleOAuthCallback(req);
 }
